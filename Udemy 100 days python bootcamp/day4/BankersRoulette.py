b2c3e015f4d8a2c660f4ebe364f463461f3010c2
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-06-17 00:07:38 +0530

    Update BankersRoulette.py

--- Udemy 100 days python bootcamp/day4/BankersRoulette.py
+++ Udemy 100 days python bootcamp/day4/BankersRoulette.py
@@ -1,7 +1,5 @@
 import random
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(",")
-payer=random.randint(0,len(names))
+payer=random.randint(0,len(names)-1)
 print(f"{names[payer]} pays the bill!")
-
-
