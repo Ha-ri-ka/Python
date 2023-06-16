5009319ff92247457d0808ac41f90fc6eae67cc5
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-06-17 00:01:22 +0530

    Delete BankersRoulette.py

--- Udemy 100 days python bootcamp/day4/BankersRoulette.py
+++
@@ -1,5 +0,0 @@
-import random
-names_string = input("Give me everybody's names, separated by a comma. ")
-names = names_string.split(",")
-payer=random.randint(0,len(names))
-print(f"{names[payer]} pays the bill!")
