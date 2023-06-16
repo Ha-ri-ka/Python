ededf8ecb2160b72c8d4eb28674a5bc4797f400c
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-06-17 00:04:04 +0530

    Update BankersRoulette.py
    Program to randomly choose a payer for the bill amongst a group of people.
    Banker's Roulette is a wordplay on Russian Roulette, a dangerous game. It origins from a culture where wealthy bankers would go on spending sprees, usually in fancy restaurants, and randomly decide on who pays the bill through a game.

--- Udemy 100 days python bootcamp/day4/BankersRoulette.py
+++ Udemy 100 days python bootcamp/day4/BankersRoulette.py
@@ -3,3 +3,5 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(",")
 payer=random.randint(0,len(names))
 print(f"{names[payer]} pays the bill!")
+
+
