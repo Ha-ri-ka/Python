38f2d142239305bf488402c0fb687465488ee107
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-06-20 01:02:40 +0530

    Create word_list.py
    A list of random words to choose from for the Hangman game program.
    
    Generated using https://chat.openai.com/

---
+++ Udemy 100 days python bootcamp/day7/word_list.py
@@ -0,0 +1,14 @@
+#contains 69 words
+words = [
+    "apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew",
+    "kiwi", "lemon", "mango", "nectarine", "orange", "pear", "quince", "raspberry",
+    "strawberry", "tangerine", "watermelon", "apricot", "blueberry", "coconut",
+    "durian", "grapefruit", "huckleberry", "jackfruit", "kumquat", "lime", "mulberry",
+    "olive", "papaya", "quinoa", "rhubarb", "salmon", "tomato", "ugli fruit",
+    "vanilla", "walnut", "xylophone", "yam", "zucchini", "acorn", "butterfly",
+    "cactus", "dolphin", "elephant", "flamingo", "giraffe", "hedgehog", "iguana",
+    "jaguar", "koala", "lemur", "mongoose", "narwhal", "ostrich", "peacock",
+    "quokka", "raccoon", "sloth", "toucan", "unicorn", "vulture", "walrus",
+    "x-ray fish", "yak", "zebra", "amethyst", "breeze", "cappuccino", "diamond",
+    "emerald", "firefly", "galaxy", "harmony", "illusion", "jazz", "kaleidoscope"
+]
