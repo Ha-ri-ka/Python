0bffd282b43118f5cfc061d2618f542120bca459
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-07-07 19:49:32 +0530

    Program to generate a spirograph.
    Each circle in the spirograph is a different color generated using random r,g,b values.

---
+++ Udemy 100 days python bootcamp/day_16_to_20/day_18/TurtleBasics/Spirograph.py
@@ -0,0 +1,21 @@
+import turtle as t
+import random
+
+def RandomColor():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    return (r,g,b)
+
+torti=t.Turtle()
+torti.shape('turtle')
+t.colormode(255)
+torti.speed("fastest")
+for _ in range(45):
+    torti.pencolor(RandomColor())
+    torti.circle(radius=100)
+    torti.right(8)
+
+    
+screen=t.Screen()
+screen.exitonclick()
