035b6ae05c0c0fd0fff13af9cc8bde2ae164c724
Harika <118831413+Ha-ri-ka@users.noreply.github.com>
2023-07-07 21:13:35 +0530

    Program to generate Damein Hirts's spot painting-esque art.
    This program attempts to produce art similar to Damein Hirst's spot paintings.
    The colorgram library is used for this to extract colors from an image and then utilizing those colors to make the art. 
    colorgram documentation: https://pypi.org/project/colorgram.py/
    Hirst's spot paintings:  https://www.google.com/search?tbm=isch&q=damein%20hirst%20spot%20paintings&tbs=imgo:1#imgrc=Z_9Yzc5elLRk2M

---
+++ Udemy 100 days python bootcamp/day_16_to_20/day_18/Art.py
@@ -0,0 +1,37 @@
+import turtle as t
+import random
+import colorgram
+artist=t.Turtle()
+artist.shape('turtle')
+t.colormode(255)
+palette=colorgram.extract('D:/python/udemy/DAY_11_TO_20/day18/20-facts-might-know-tangled.jpg',10)
+colors=[]
+for i in range(len(palette)):
+    colors.append(palette[i].rgb)
+artist.hideturtle()
+artist.penup()
+x = -t.window_width() / 2
+y = t.window_height() / 2
+artist.setpos(x+50,y-25)
+artist.pendown()
+
+for j in range(16):
+    if j%2==0 and j!=0:
+        artist.left(90)
+        artist.penup()
+        artist.forward(40)
+        artist.left(90)
+        artist.forward(50)
+    if j%2!=0 and j!=0:
+        artist.right(90)
+        artist.penup()
+        artist.forward(40)
+        artist.right(90)
+        artist.forward(50)
+    for i in range(14):
+        artist.dot(15,random.choice(colors))
+        artist.penup()
+        artist.forward(50)
+
+screen=t.Screen()
+screen.exitonclick()
